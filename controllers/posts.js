2841930ce0d80cd561cffe3f71e272f7db7183e8
nichro02 <nichro02@gmail.com>
2020-11-07 17:08:03 -0800

    Pulled data from API call to populate new post ejs. Next step is to add form to page for user text entry

--- controllers/posts.js
+++ controllers/posts.js
@@ -8,12 +8,24 @@ router.post('/', (req, res) => {
     const unsplashUrl = `https://api.unsplash.com/photos/random/?client_id=${process.env.access_key}`
     axios.get(unsplashUrl)
     .then(function(apiResponse) {
+        const data = {}
         photo = apiResponse.data
-        console.log(photo)
-        res.send('NEW POST ROUTE')
+        data.imageUrl = photo.urls.regular
+        data.photographerName = photo.user.name
+        data.photographerLink = photo.links.html
+        console.log('API RESPONSE',photo)
+        console.log('HOTLINK', photo.urls.regular)
+        console.log('PHOTOGRAPHER NAME', photo.user.name)
+        console.log('PHOTOGRAPHER LINK', photo.user.links.html)
+        //res.send('NEW POST ROUTE')
+        res.render('posts/new.ejs', data)
     })
     
 })
+//Limit of 50 API requests/hour
+//Photos need to be hotlinked
+//Need to attribute Unsplash, the photographer, and a link to the photographer's Unsplash profile
+//https://help.unsplash.com/en/articles/2511315-guideline-attribution
 
 
 //GET /posts - display form to create new post

f19135067f4b7eb3a906eb79e872d2b1bf66ac21
nichro02 <nichro02@gmail.com>
2020-11-07 16:19:40 -0800

    Added API to new post route

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,13 +1,25 @@
 let express = require('express')
 const router = express.Router()
 let db = require('../models')
+const axios = require('axios')
 
 //POST /posts - create a new post
 router.post('/', (req, res) => {
-    res.send('NEW POST ROUTE')
+    const unsplashUrl = `https://api.unsplash.com/photos/random/?client_id=${process.env.access_key}`
+    axios.get(unsplashUrl)
+    .then(function(apiResponse) {
+        photo = apiResponse.data
+        console.log(photo)
+        res.send('NEW POST ROUTE')
+    })
+    
 })
 
-//GET /posts/new - display form to create new post
+
+//GET /posts - display form to create new post
+router.get('/', (req, res) => {
+    res.send('VIEW POSTS ROUTE')
+})
 
 //GET /posts/:id - display a specific post
 
